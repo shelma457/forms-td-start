14a8a81d15d93026982bd72b3616db5a0c0ba8cd
shelma <shelma457@gmail.com>
2018-02-15 21:16:17 +0530

    second commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  @ViewChild('f') signupForm:NgForm;
+  defaultQuestion = 'teacher';
+  answer:string;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  /*onSubmit(form:NgForm){
+    console.log(form);
+  }*/
+
+  onSubmit(){
+    console.log(this.signupForm);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
